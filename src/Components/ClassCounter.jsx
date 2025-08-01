16d4801ebf337b4b3df47a6d01719b0b33c358c2
RSPInfoTech <89846204+RSPTechSolution@users.noreply.github.com>
2025-08-01 12:18:52 +0530

    Update ClassCounter.jsx

--- src/Components/ClassCounter.jsx
+++ src/Components/ClassCounter.jsx
@@ -9,8 +9,8 @@ class ClassCounter extends Component {
                     <h1>Class Component</h1>
                     <div className="label">{this.state.count}</div>
                     <div className="action-button">
-                        <button onClick={() => this.setState({ count: this.state.count + 1})}>➕</button>
-                        <button onClick={() => this.state.count > 0 && this.setState({count: this.state.count - 1})}>➖</button>
+                        <button onClick={() => this.setState({ count: this.state.count + 1})}>+</button>
+                        <button onClick={() => this.state.count > 0 && this.setState({count: this.state.count - 1})}>-</button>
                     </div>
                 </div>
             </>
